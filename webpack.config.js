7b4c6fc06bd80272386b5f764cfbb7d6a961dd2d
Luiz Américo <brandkarl@gmail.com>
2016-01-05 21:24:45 -0300

    Update dependencies and change webpack config / test script to match babel 6 reuirements

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,10 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: '/node_modules/',
-        loader: 'babel-loader'
+        loader: 'babel',
+		query: {
+              presets: ['es2015']
+            }
       }
     ]
   },
